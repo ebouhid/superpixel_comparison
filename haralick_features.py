ff4d40e09b2db4e8ae13c05bbb3cce09f4a5d7c6
Eduardo Bouhid <eduardobouhid.neto@gmail.com>
2024-05-05 23:50:33 -0300

    feat: handle cases when segment is too 'thin' to extract haralick features

--- haralick_features.py
+++ haralick_features.py
@@ -4,6 +4,12 @@ import numpy as np
 def get_haralick_features(image):
     features = []
     for channel in range(image.shape[2]):
-        features.append(mahotas.features.haralick(image[:, :, channel]))
+        try:
+            features.append(mahotas.features.haralick(image[:, :, channel]))
+        except ValueError as e:
+            print(f"Error in channel {channel}")
+            print(f"Image shape: {image.shape}")
+            print(f"Unique values: {np.unique(image[:, :, channel])}")
+            features.append(np.zeros((4, 13)))
     return np.array(features)
 
