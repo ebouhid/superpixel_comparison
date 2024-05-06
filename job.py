cfd86cfb102aad7fedd324c41924b49fa1cfea86
Eduardo Bouhid <eduardobouhid.neto@gmail.com>
2024-05-05 23:51:48 -0300

    feat: included a job wrapper script that evaluates all combinations sequentially (might be parallel later)

---
+++ job.py
@@ -0,0 +1,34 @@
+from deforestation_detection import train_and_evaluate
+from itertools import product
+
+if __name__ == '__main__':
+    # Define parameters
+    methods = [
+        "CRS",
+        "DISF",
+        "ERGC",
+        "ERS",
+        "ETPS",
+        "GRID",
+        "IBIS",
+        "ISF",
+        "LNSNet",
+        "LSC",
+        "ODISF",
+        "RSS",
+        "SEEDS",
+        "SH",
+        "SICLE", # um salve pro felipinho
+        "SIN",
+        "SLIC",
+        "SNIC",
+        "SSFCN"
+    ]
+
+    n_seg_parameters = [25, 50, 75, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+
+    for method, n_seg in product(methods, n_seg_parameters):
+        train_and_evaluate(method, n_seg, [3, 2, 1], 42, 'scenes_allbands_ndvi', 'truth_masks')
+        print(f"Finished {method} with {n_seg} segments!")
+    
+    print("Finished all jobs!")
