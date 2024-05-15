c9f26711b880fde0242030a70dcaf88a4aa499ff
Eduardo Bouhid <eduardobouhid.neto@gmail.com>
2024-05-15 20:03:15 -0300

    Updated test script

--- test_deforestation_svm.py
+++ test_deforestation_svm.py
@@ -1,4 +1,5 @@
 from deforestation_detection import train_and_evaluate
 
 if __name__ == "__main__":
-    train_and_evaluate('CRS', '300', [3, 2, 1], 42, 'scenes_allbands_ndvi', 'truth_masks')
+    train_and_evaluate('CRS', '800', [3, 2, 1], 42, 'scenes_allbands_ndvi', 'truth_masks')
+    train_and_evaluate('ETPS', '50', [3, 2, 1], 42, 'scenes_allbands_ndvi', 'truth_masks')
